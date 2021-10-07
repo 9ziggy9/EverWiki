363708f8af3f7003ade846303df9c2392f59567c
Ziggy <davidarogers@protonmail.com>
2021-10-07 13:38:58 -0700

    login testing complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 // testing routes
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// const { restoreUser } = require('../../utils/auth.js');
+// const { requireAuth } = require('../../utils/auth.js');
 // router.post('/test', function(req, res) {
 //   res.json({ requestBody: req.body });
 // });
@@ -35,5 +37,13 @@ const { requireAuth } = require('../../utils/auth.js');
 //     return res.json(req.user);
 //   }
 // );
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
