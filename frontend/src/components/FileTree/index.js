ee3f00e5361eadcfa80e5bee890ee7fe675ea6b5
Ziggy <davidarogers@protonmail.com>
2021-10-11 17:13:31 -0700

    fixed stylesheet redundancies; PAY CLOSER ATTENTION

--- frontend/src/components/FileTree/index.js
+++ frontend/src/components/FileTree/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import './FileTree.css'
@@ -6,12 +6,16 @@ import { NavLink } from 'react-router-dom';
 
 function FileTree() {
   const sessionUser = useSelector(state => state.session.user);
+  const [showFileTree, setShowFileTree] = useState(true);
+
   if (sessionUser) (
     console.log(`Logged in as ${sessionUser}; hello from FileTree.`)
   );
+
   return (
-    <>
-    </>
+    <div>
+      { sessionUser && <h1>Hello from File Tree</h1> }
+    </div>
   );
 }
 
