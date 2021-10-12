10689c6190cf9920957fcce985b6ef098b248acd
Ziggy <davidarogers@protonmail.com>
2021-10-12 10:45:06 -0700

    note post route established

--- backend/routes/api/notes.js
+++ backend/routes/api/notes.js
@@ -11,17 +11,39 @@ const validateNote = [
   check('title')
     .exists({ checkFalsy: true })
     .withMessage('Please provide a valid title.'),
+  check('content')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a valid note.'),
   handleValidationErrors,
 ];
 
+// Note get route.
+// NOTE: requireAuth implies that user must be logged in to access ANY notes.
 router.get(
   '/:id(\\d+)',
+  requireAuth,
   asyncHandler(async (req,res) => {
     const noteId = parseInt(req.params.id, 10);
     const note = await Note.findByPk(noteId);
     console.log("HELLO FROM NOTE ROUTE:", noteId);
-    console.log(note);
+    console.log("USER ID IS:", req.user.id);
   }),
+);
+
+router.post(
+  '/',
+  requireAuth,
+  validateNote,
+  asyncHandler(async (req,res) => {
+    const userId = req.user.id;
+    const {title,content} = req.body;
+    const note = await Note.create({
+        userId,
+        title,
+        content
+      });
+    return res.json(note);
+  })
 );
 
 module.exports = router;
