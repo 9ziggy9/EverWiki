222ff798839993ca592432be0ccc889d2c85968e
Ziggy <davidarogers@protonmail.com>
2021-10-12 15:38:57 -0700

    Correction to bulleting issues

--- frontend/src/components/TextEditor/Editor.js
+++ frontend/src/components/TextEditor/Editor.js
@@ -3,12 +3,20 @@ import React from 'react';
 function Editor({text, setText, textStream, setTextStream}) {
 
   const splitOnNewLines = (string) => string.split('\n');
-  const parseBullets = (array) =>
-        array.map(e => e[0] === '*' ? `<h2>${e.substring(1)}</h2>` : e);
+  const parseBullets = (array) => array.map(e => {
+    switch (e[0]) {
+      case '*':
+        let n = 0;
+        while(e[n] === '*') n++;
+        return `<h${n+2}>${e.slice(n)}</h${n+2}>`;
+      default:
+        return `<p>${e}</p>`;
+    }
+  });
 
   const editText = (event) => {
     setText(event.target.value);
-    setTextStream(parseBullets(splitOnNewLines(event.target.value)));
+    setTextStream(parseBullets(splitOnNewLines(event.target.value)).join(''));
   }
 
   return (
