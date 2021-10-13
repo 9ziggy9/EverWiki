1f182004183421677f3f0b58aa27848bf0d0e113
Ziggy <davidarogers@protonmail.com>
2021-10-13 11:31:20 -0700

    cheating real quick.

--- frontend/src/components/TextEditor/Preview.js
+++ frontend/src/components/TextEditor/Preview.js
@@ -2,14 +2,9 @@ import React from 'react';
 import katex from 'katex';
 
 function Preview({text, textStream}) {
-  // const katexTest = katex.renderToString("\\oint_{\\partial \\Sigma} \\omega = \\int_{\\Sigma} d\\omega", {
-  //   displayMode: true,
-  //   throwOnError: false
-  // });
   return (
     <>
       <div dangerouslySetInnerHTML={{__html: textStream}} />
-      {/* <div dangerouslySetInnerHTML={{__html: katexTest}} /> */}
     </>
   );
 }
